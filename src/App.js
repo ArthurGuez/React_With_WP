61b910c4868b83d39c38b8e3cd821c32d10f8feb
Arthur Guez <arthurguez29@gmail.com>
2020-11-09 11:53:04 +0100

    some linting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Pizzas from "./components/Pizzas";
-import PizzaPage from "./components/PizzaPage";
+import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Pizzas from './components/Pizzas';
+import PizzaPage from './components/PizzaPage';
 
 function App() {
-
-  return (
-    <Router>
-    <Fragment>
-      <Route exact path="/" component={Pizzas} />
-      <Route exact path="/post/:pizzaId" component={PizzaPage} />
-    </Fragment>
-    </Router>
-  );
+	return (
+		<Router>
+			<Fragment>
+				<Route exact path="/" component={Pizzas} />
+				<Route exact path="/post/:pizzaId" component={PizzaPage} />
+			</Fragment>
+		</Router>
+	);
 }
- 
+
 export default App;
--- src/components/Pizzas.js
+++ src/components/Pizzas.js
@@ -1,37 +1,36 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import PizzaItem from './PizzaItem';
 import axios from 'axios';
 
-
 function Pizzas() {
-    const [pizzas, setPizzas] = useState([]);
-    const [isLoading, setIsLoading] = useState(false);
+  const [pizzas, setPizzas] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    useEffect(() => {
+  useEffect(() => {
     const fetchPizzas = async () => {
-        setIsLoading(true);
-        const res = await axios('/wp-json/wp/v2/pizzas/');
-        setPizzas(res.data);
-        setIsLoading(false);
+      setIsLoading(true);
+      const res = await axios('/wp-json/wp/v2/pizzas/');
+      setPizzas(res.data);
+      setIsLoading(false);
     };
 
     fetchPizzas();
-    }, []);
+  }, []);
 
-    return (
-        <div>
-            <h1>Les Pizzas :</h1>
-            {isLoading ? (
-                <div>Loading...</div>
-            ) : (
-                <ul>
-                {pizzas.map(pizza => (
-                    <PizzaItem key={pizza.id} pizza = {pizza}/>
-                ))}
-                </ul>
-            )}
-        </div>
-    )
+  return (
+    <div>
+      <h1>Les Pizzas :</h1>
+      {isLoading ? (
+        <div>Loading...</div>
+      ) : (
+        <ul>
+          {pizzas.map((pizza) => (
+            <PizzaItem key={pizza.id} pizza={pizza} />
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 }
 
 export default Pizzas;
